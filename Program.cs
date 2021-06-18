cc2ffcd1040ffb87966f8c9ccb0e4cb89c527e96
Amitash Kaur <Amitashkaur8632@conestogac.on.ca>
2021-06-17 23:23:32 -0400

    Completed Rectangle Class

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _8040_Assignment01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
